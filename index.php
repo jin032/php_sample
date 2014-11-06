fe643c5cab648a05306ab204a37daeab740925b2
saddam <jin032@yahoo.com>
2014-11-06 20:00:21 +0500

    add form

--- index.php
+++ index.php
@@ -1,7 +1,31 @@
-<?php
-echo "hello saddam hi!";
+<?php	    		
+if ($_POST["submit"]) {	        									
+	if (!$_POST['name']) {	            				
+		$error="<br />Please enter your name";	            			
+	}						
+		if (!$_POST['email']) {	            				
+			$error.="<br />Please enter your email address";	            			
+		}						if (!$_POST['comment']) {	            				
+			$error.="<br />Please enter a comment";	            			
+		}				
+		if ($_POST['email']!="" AND !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) 
+			{	    			    		
+				$error.="<br />Please enter a valid email address";	            			
+			}						
+			if ($error) {	            	           
+			 $result='<div class="alert alert-danger"><strong>There were error(s) in your form:</strong>'.$error.'</div>';	            			
+			} else {	            	            
+				if (mail("test@greenhost.org.uk", "Comment from website!", "Name: ". $_POST['name']."	                     	                     
+					Email: ".$_POST['email']."	                     	                     
+					Comment: ".$_POST['comment'])) {	                     	                     
+					$result='<div class="alert alert-success"><strong>Thank you!</strong> I\'ll be in touch.</div>';	                     	                     
+			} else {	                     	                    
+			 $result='<div class="alert alert-danger">Sorry, there was an error sending your message. Please try again later.</div>';	                     	                     
+			}	
+     }
 
-?>
+       }
+?>	
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -16,6 +40,14 @@ echo "hello saddam hi!";
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap-theme.min.css">
     <style type="text/css">
+    .emailForm {	
+    	border:1px solid grey;	    
+    	border-radius:10px;	    
+    	margin-top:20px;	
+    	}	 
+    	form {	    
+    		padding-bottom:20px;			
+    	}
 
     body{
 
@@ -105,6 +137,29 @@ echo "hello saddam hi!";
                   phone no.03007446216
                   Email:jin032@yahoo.com
                   if urgent then call me....otherwise email is the best way to reach me.....
+                  <div class="container">
+                   <div class="row">
+                    <div class="col-md-6 col-md-offset-3 emailForm">	
+                    	<h1>My email form</h1>	
+				 <?php echo $result; ?>	 
+				 <p class="lead">Please get in touch - I'll get back to you as soon as I can.</p>
+				  <form method="post">	! <div class="form-group">
+				   <label for="name">Your Name:</label>
+				   	<input type="text" name="name" class="form-control" placeholder="Your Name" value="<?php echo $_POST['name']; ?>" />
+				   </div>
+				    <div class="form-group">
+				     <label for="email">Your Email:</label>
+				     	<input type="email" name="email" class="form-control" placeholder="Your Email"  value="<?php echo $_POST['email']; ?>" />
+				     	 </div>	
+				     	 <div class="form-group">
+				     	  <label for="comment">Your Comment:</label>	
+				     	  <textarea class="form-control" name="comment"><?php echo $_POST['comment']; ?></textarea>
+</div>
+ <input type="submit" name="submit" class="btn btn-success btn-lg" value="Submit" />
+</form>
+</div>
+</div>
+</div>	! 
 
               </div>
               <div class="footer">Saddam Akram</div>
